ab7da437469cf1ee9a847208d5c4d391ddb04f61
ozubergs <34900230+ozubergs@users.noreply.github.com>
2018-11-01 08:42:52 -0400

    Updates

---
+++ precache-manifest.7514a4b2fb827bb1933d6c3213569728.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae265bfad00735e37c33",
+    "url": "/random-quote/static/js/runtime~main.ae265bfa.js"
+  },
+  {
+    "revision": "52675909df6f26129d31",
+    "url": "/random-quote/static/js/main.52675909.chunk.js"
+  },
+  {
+    "revision": "e240ab884a88ac205823",
+    "url": "/random-quote/static/js/1.e240ab88.chunk.js"
+  },
+  {
+    "revision": "d10f47689d15b6fa51ce3353ae5aac62",
+    "url": "/random-quote/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import QuoteReducer from './reducer_quote';
-
-const rootReducer = combineReducers({
-  quote: QuoteReducer
-});
-
-export default rootReducer;
